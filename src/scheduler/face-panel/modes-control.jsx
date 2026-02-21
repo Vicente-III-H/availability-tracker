78fd2f356465f291b63063d70cab546b7a96e91d
V H <145728917+Vicente-III-H@users.noreply.github.com>
2026-02-21 13:57:40 -0500

    Finish Modes dropdown basic functionality and basic styling

--- src/scheduler/face-panel/modes-control.jsx
+++ src/scheduler/face-panel/modes-control.jsx
@@ -1,38 +1,53 @@
+import { useState } from "react";
 import { severities } from "../scheduler-types";
 import { VIEW_DEFAULT } from "../scheduler-types";
+import './modes-control.css'
 
 function DropdownDivider() {
     return (
-        <div></div>
+        <div className="dropdown-divider"></div>
     )
 }
 
 function DropdownOption({ name, setMode }) {
     return (
-        <button onClick={() => {setMode(name)}}>
+        <button className="dropdown-option" onClick={() => {setMode(name)}}>
             {name}
         </button>
     )
 }
 
+function Dropdown({ setMode }) {
+    return (
+        <div className="dropdown-menu">
+            <DropdownOption name={"View"} setMode={setMode} />
+            <DropdownDivider />
+            {severities.toReversed().map((severity) => <DropdownOption name={severity} setMode={setMode} key={severity}/>)}
+            <DropdownDivider />
+            <DropdownOption name={"Free"} setMode={setMode} />
+        </div>
+    )
+}
+
 function ModesControl({ mode, view }) {
+    const [showDropdown, setShowDropdown] = useState(false);
+
+    const toggleDropdown = () => {
+        setShowDropdown(!showDropdown);
+    }
+
     const setMode = (value) => {
+        setShowDropdown(false);
         mode.set(value);
         view.set(VIEW_DEFAULT);
     }
 
     return (
-        <div>
+        <div id="modes">
             <div>{mode.current + " - " + view.current}</div>
-            <div id="modes-dropdown">
-                <button>{mode.current}</button>
-                <div id="dropdown-options">
-                    <DropdownOption name={"View"} setMode={setMode} />
-                    <DropdownDivider />
-                    {severities.toReversed().map((severity) => <DropdownOption name={severity} setMode={setMode} key={severity}/>)}
-                    <DropdownDivider />
-                    <DropdownOption name={"Free"} setMode={setMode} />
-                </div>
+            <div className="dropdown">
+                <button onClick={toggleDropdown}>{mode.current}</button>
+                {showDropdown ? <Dropdown setMode={setMode} /> : null}
             </div>
         </div>
     )
