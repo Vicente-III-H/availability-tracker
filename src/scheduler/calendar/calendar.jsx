80ac6856fc5d77a9ca22ba940645f2ee3a9d560c
V H <145728917+Vicente-III-H@users.noreply.github.com>
2026-02-21 09:27:32 -0500

    Generate days in calendar

--- src/scheduler/calendar/calendar.jsx
+++ src/scheduler/calendar/calendar.jsx
@@ -1,13 +1,17 @@
 import { weekdayAbbreviation } from "../../global";
 
-function Day() {
+function Day({ date, disabled = false }) {
+    if (disabled) {
+        return <div className="day disabled"></div>
+    }
+
     return (
-        <div></div>
+        <div className="day">{date}</div>
     )
 }
 
 function Calendar({ People, CalendarInfo, viewAvailability }) {
-    const daysInfo = (() => {
+    const daysList = (() => {
         const firstDayOffset = (new Date(CalendarInfo.year(), CalendarInfo.month())).getDay();
         const totalDaysInMonth = (new Date(CalendarInfo.year(), CalendarInfo.month() + 1, 0)).getDate();
 
@@ -15,11 +19,24 @@ function Calendar({ People, CalendarInfo, viewAvailability }) {
         const weekForRemainingDays = (firstDayOffset + totalDaysInMonth) % 7 > 0 ? 1 : 0;
         const numberOfWeeks = numberOfFullWeeks + weekForRemainingDays;
 
-        return {
-            firstDayOffset,
-            totalDaysInMonth,
-            numberOfWeeks
+        const daysList = [];
+        for (let i = 0; i < numberOfWeeks * 7; i++) {
+            const dayObj = {
+                id: crypto.randomUUID()
+            }
+
+            if (i < firstDayOffset) {
+                dayObj.day = "disabled";
+            } else if (i < firstDayOffset + totalDaysInMonth) {
+                dayObj.day = (i - firstDayOffset + 1).toString();
+            } else {
+                dayObj.day = "disabled";
+            }
+
+            daysList.push(dayObj);
         }
+
+        return daysList;
     })();
 
     return (
@@ -27,7 +44,14 @@ function Calendar({ People, CalendarInfo, viewAvailability }) {
             <div>{CalendarInfo.monthName()}</div>
             <div id="weekdays">{weekdayAbbreviation.map((weekday) => { return <div key={weekday}>{weekday}</div> })}</div>
             <div id="days">
-                
+                {
+                    daysList.map((dayObj) => {
+                        if (dayObj.day === "disabled") {
+                            return <Day disabled={true} key={dayObj.id}></Day>
+                        }
+                        return <Day date={dayObj.day} key={dayObj.id}></Day>
+                    })
+                }
             </div>
         </div>
     )
